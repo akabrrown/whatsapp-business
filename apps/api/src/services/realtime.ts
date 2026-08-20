c87041acda308c5bfd8357c79ee1006baa764b3f
Software-technolgy-team <akayetb@gmail.com>
2026-08-20 21:18:55 +0000

    fix(api): provide flexible type signatures on dummy hub for serverless build

--- apps/api/src/services/realtime.ts
+++ apps/api/src/services/realtime.ts
@@ -1,7 +1,17 @@
 // Dummy hub since WebSockets have been removed for Vercel Serverless migration.
-export const hub = {
+export const hub: {
+  log: any[];
+  broadcast: (...args: any[]) => void;
+  broadcastAdmin: (...args: any[]) => void;
+  sendToWhatsApp: (...args: any[]) => void;
+  sendToWeb: (...args: any[]) => void;
+  attach: (...args: any[]) => void;
+  [key: string]: any;
+} = {
   log: [] as any[],
-  broadcast: (event: any) => { hub.log.push(event); },
-  sendToWhatsApp: (num: any, ev: any) => { hub.log.push({ to: num, ...ev }); },
-  sendToWeb: (sess: any, ev: any) => { hub.log.push({ to: sess, ...ev }); },
+  broadcast: (...args: any[]) => { hub.log.push(args); },
+  broadcastAdmin: (...args: any[]) => { hub.log.push(args); },
+  sendToWhatsApp: (...args: any[]) => { hub.log.push(args); },
+  sendToWeb: (...args: any[]) => { hub.log.push(args); },
+  attach: (...args: any[]) => {},
 };
